9fcfb5e5aecc23fd3972fc7cf7520b66b83f1113
Lau <lauxsecure@gmail.com>
2024-06-19 19:45:58 +0200

    Manglende filer

---
+++ app/src/main/java/com/example/beerbud/models/Beer.kt
@@ -0,0 +1,14 @@
+package com.example.beerbud.models
+
+data class Beer(
+    val id: Int,
+    val user: String,
+    val brewery: String,
+    val name: String,
+    val style: String,
+    val abv: Double,
+    val volume: Int,
+    val pictureUrl: String,
+    val howMany: Int
+)
+
